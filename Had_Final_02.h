707e34095d46c11cfc4d5c39ea6d90ff634e0bde
lmichalik15 <119747293+lmichalik15@users.noreply.github.com>
2022-12-06 19:14:17 +0100

    Add files via upload

---
+++ Had_Final_02.h
@@ -0,0 +1,55 @@
+﻿// CMakeProject1.h: Soubor k zahrnutí pro standardní systémové soubory k zahrnutí
+// nebo soubory k zahrnutí specifické pro projekt.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Zde odkažte na dodatečné hlavičky, které program vyžaduje.
+
+
+// vyvhozi hodnoty pro aplikaci
+
+#define widthField 30         // sirka pole
+#define heightField 15        // vyska pole
+
+int field[heightField][widthField];    // pole
+
+// pozice hada, hlavy, ocasu
+int x;
+int y;
+int Gy;
+int head;
+int tail;
+
+int i;
+int j;
+int game;
+
+// ovoce  // pozice ovoce
+int fruit;
+int a;
+int b;
+
+// prekazky
+int obstacle;
+int l;
+int m;
+
+
+int var;         // klavesa
+int direction;   // smer
+int difficultness;
+int speed;
+
+
+// bodovani
+char name[32];
+int score;
+#define MAX_LINE_LENGTH 1000
+
+// sipky
+int doprava = 77;
+int doleva = 75;
+int dolu = 80;
+int nahoru = 72;
