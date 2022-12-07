88bff4d3d20c73585b91e1d2bf64d294af58811a
lmichalik15 <119747293+lmichalik15@users.noreply.github.com>
2022-12-07 14:10:13 +0100

    Update Had_Final_02.h

--- Had_Final_02.h
+++ Had_Final_02.h
@@ -1,10 +1,3 @@
-﻿// CMakeProject1.h: Soubor k zahrnutí pro standardní systémové soubory k zahrnutí
-// nebo soubory k zahrnutí specifické pro projekt.
-
-#pragma once
-
-#include <iostream>
-
 // TODO: Zde odkažte na dodatečné hlavičky, které program vyžaduje.
 
 
